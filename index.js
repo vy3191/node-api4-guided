4aa1c2747f224eb6f044b9496a223860e7a6a06c
vy3191 <venkyafhs@gmail.com>
2020-02-19 21:02:38 -0500

    initial commit & added .env file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const welcomeRouter = require("./welcome/welcome-router")
 const shoutsRouter = require("./shouts/shouts-router")
 
 const server = express()
-const port = 4000
+const port = process.env || 4000;
 
 server.use(helmet())
 server.use(express.json())
@@ -14,7 +14,7 @@ server.use("/shouts", shoutsRouter)
 server.use((err, req, res, next) => {
 	console.log(err)
 	res.status(500).json({
-		message: "Something went wrong",
+		message: process.env.SECRET_KEY || "Well, your secret no longer works now.",
 	})
 })
 
